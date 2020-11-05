7fa0da957d65a48469fe1e791207eb770ad440bb
Alfie Froude <alfiefroude111@gmail.com>
2020-11-05 14:10:27 +0000

    Update to syntax

--- Assignment-1/Blackjack Card Game.py
+++ Assignment-1/Blackjack Card Game.py
@@ -19,16 +19,16 @@ def play():
     print("***Insert Gameplay Here***")
     global players_hand
     players_hand = []
-    print("You have been delt 2 cards:")
+    print("You have been dealt 2 cards:")
     deal_hand()
 
 def deal_hand():
     initial_deal_amount = 2
     for i in range(initial_deal_amount):
-        card_delt = one_shuffled_deck(1)
-        print(card_delt)
-        players_hand.append(card_delt)
-        #the peramiter inputted is the number of cards delt initially in blackjack
+        card_dealt = one_shuffled_deck(1)
+        print(card_dealt)
+        players_hand.append(card_dealt)
+        #the peramiter inputted is the number of cards dealt initially in blackjack
 
 #def deal():
 
@@ -74,7 +74,7 @@ def one_shuffled_deck(x):
 def rules():
     print("Aim:\n Get your hand's total as close to 21 as possible.\n If you exceed 21 you are bust.\n If you get 21 you have blackjack.\n  - In this case, you can only be beat if the dealer gets blackjack aswell. (This results in a DRAW).\n If you get anything below blackjack you just need to get higher than the dealer to WIN.\n")
     print("How To Play: ")
-    print("1) You will be delt 2 cards\n2) You have 2 options, FOLD or DRAW\n  FOLD: Stop your play\n  DRAW: Draw another card\n3) Get your hand value to 21 or higher than the dealers hand\n4) Don't get BUST! ")
+    print("1) You will be dealt 2 cards\n2) You have 2 options, FOLD or DRAW\n  FOLD: Stop your play\n  DRAW: Draw another card\n3) Get your hand value to 21 or higher than the dealers hand\n4) Don't get BUST! ")
     
     input("\nPress Any Key To Continue...")
     start()
