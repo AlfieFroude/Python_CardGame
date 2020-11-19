44330bb7cbca2b838afe0daedebc2aa62785b211
Alfie Froude <alfiefroude111@gmail.com>
2020-11-19 13:01:32 +0000

    Game Logic Updated
    The player can now choose deal() or Fold(), if they fold the "dealer" then plays. The dealer logic is simple at the moment. The ace rule is also not implemented yet.

--- Assignment-1/Blackjack Card Game.py
+++ Assignment-1/Blackjack Card Game.py
@@ -4,6 +4,8 @@ import random
 from os import sys
 
 deck_amount = 53
+global dealer_bust
+dealer_bust = False
 
 def start():
     print("\n~~~Welcome to Blackjack!~~~\n")
@@ -18,6 +20,8 @@ def start():
         start()
 
 def play():
+    global turn
+    turn = True
     global players_hand
     players_hand = []
     global hand_value
@@ -36,6 +40,8 @@ def gameplay():
         if (nextmove == "d"):
             deal()
         elif (nextmove == "f"):
+            global turn
+            turn = False
             fold()
             break
     else:
@@ -70,13 +76,12 @@ def fold():
     for i in players_hand:
         print(i, end=" ")
     print("\n\nThe total of your hand is: " + str(hand_value))
-    print("\n\nThe Dealer got: " + str(dealers_hand_total))
+    the_dealer()
 
 def bust():
     if (hand_value > 21):
         lose_bust()
         
-
 
 def one_shuffled_deck():
 
@@ -119,14 +124,67 @@ def one_shuffled_deck():
         card_value == 11
     else:
         card_value = int(card_value)
-    global hand_value
-    hand_value = hand_value + card_value
+    if (turn == True):
+        global hand_value
+        hand_value = hand_value + card_value
+    elif (turn == False):
+        global dealers_hand_value
+        dealers_hand_value = dealers_hand_value + card_value
     return card_picked
 
         #when function is called a random card to drawn from the deck
 
 def the_dealer():
-    
+    global dealers_hand
+    dealers_hand = []
+    global dealers_hand_value
+    dealers_hand_value = 0
+    dealer_deal_hand()
+    dealers_gameplay_logic()
+
+def dealers_gameplay_logic():
+    while (dealers_hand_value < 21):
+        if (dealers_hand_value <= 16):
+            dealer_deal()
+        else:
+            dealer_fold()
+            break
+    else:
+        global dealer_bust
+        dealer_bust = True
+        dealer_fold()
+
+
+def dealer_deal_hand():
+    i = 0
+    initial_deal_amount = 2
+    while(i != initial_deal_amount):
+        card_dealt = one_shuffled_deck()
+        dealers_hand.append(card_dealt)
+        #the peramiter inputted is the number of cards dealt initially in blackjack
+        i = i + 1
+
+def dealer_deal():
+    card_dealt = one_shuffled_deck()
+    dealers_hand.append(card_dealt)
+    #print(hand_value)
+    dealers_gameplay_logic()
+
+def dealer_fold():
+    global hand_value
+    global dealers_hand_value
+    global dealer_bust
+    print("\nThe Dealer got: " + str(dealers_hand_value))
+    if (dealer_bust == True) & (hand_value <= 21):
+        win()
+    else:
+        if (hand_value == dealers_hand_value):
+            draw()
+        elif (hand_value > dealers_hand_value ):
+            win()
+        elif (hand_value < dealers_hand_value):
+            lose()
+
 
 def rules():
     print("Aim:\n Get your hand's total as close to 21 as possible.\n If you exceed 21 you are bust.\n If you get 21 you have blackjack.\n  - In this case, you can only be beat if the dealer gets blackjack aswell. (This results in a DRAW).\n If you get anything below blackjack you just need to get higher than the dealer to WIN.\n")
@@ -147,6 +205,11 @@ def draw():
 
 def lose_bust():
     print("\n\nYou got BUST!\n\n***YOU LOSE***")
+    input("\n\n Press Any Key To Exit...")
+    end()
+
+def lose():
+    print("\n\nThe dealer beat you!\n\n***YOU LOSE***")
     input("\n\n Press Any Key To Exit...")
     end()
 
