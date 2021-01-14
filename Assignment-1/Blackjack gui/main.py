cc2d68c155225383c50539f22f990f9926256b04
Alfie Froude <alfiefroude111@gmail.com>
2021-01-14 12:18:11 +0000

    Trying Pygame to create GUI
    I couldn't get the gui to work with Tkinter so I have opted to try Pygame

---
+++ Assignment-1/Blackjack gui/main.py
@@ -0,0 +1,33 @@
+import pygame
+import os
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Blackjack Game - Alfie Froude")
+
+GREEN = (10, 108, 3)
+
+FPS = 60
+
+CARD = pygame.image.load(os.path.join('Assets', '2C.png'))
+
+def draw_window():
+    WIN.fill(GREEN)
+    WIN.blit(CARD, (300, 100))
+    pygame.display.update()
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        draw_window()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
